e3fef89d8fb09c4e5466440a1c1eaf46f6137c88
@maxgfr <maxgolfier@gmail.com>
2019-06-04 16:04:29 +0200

    push

--- analysis/build.sbt
+++ analysis/build.sbt
@@ -8,7 +8,15 @@ ThisBuild / organizationName := "maxgfr"
 lazy val root = (project in file("."))
   .settings(
     name := "analysis",
-    libraryDependencies += scalaTest % Test
+    libraryDependencies ++= Seq(
+      scalaTest % Test,
+      "net.debasishg" %% "redisclient" % "3.9",
+      "com.redislabs" % "spark-redis" % "2.4.0",
+      "org.apache.spark" %% "spark-core" % "2.4.3",
+      "org.apache.spark" %% "spark-sql" % "2.4.3",
+      "org.apache.spark" %% "spark-catalyst" % "2.4.3",
+      "org.apache.spark" %% "spark-streaming" % "2.4.3"
+    )
   )
 
 // See https://www.scala-sbt.org/1.x/docs/Using-Sonatype.html for instructions on how to publish to Sonatype.
--- analysis/src/main/scala/maxgfr/Main.scala
+++ analysis/src/main/scala/maxgfr/Main.scala
@@ -1,9 +1,37 @@
-package example
+package maxgfr
 
-object Hello extends Greeting with App {
-  println(greeting)
-}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import com.redislabs.provider.redis._
 
-trait Greeting {
-  lazy val greeting: String = "hello world"
+object Main extends App {
+
+    val spark = SparkSession
+              .builder()
+              .appName("redis-analysis")
+              .master("local[*]")
+              .config("spark.redis.host", "redis")
+              .config("spark.redis.port", "6379")
+              .getOrCreate()
+
+    val str = spark
+                .readStream
+                .format("redis")
+                .option("stream.keys","stream")
+                .schema(StructType(Array(
+                      StructField("data", StringType)
+                 )))
+                 .load()
+
+    val dataframe = str.groupBy("data").count
+
+    val query = dataframe
+               .writeStream
+               .outputMode("update")
+               .format("console")
+               .start()
+
+    query.awaitTermination()
+
 }
